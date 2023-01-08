f5e5057f63dbfc36ee6d243839db06616623943b
Albert <d.albert015@yahoo.com>
2023-01-08 19:08:08 +0200

    change behaviour for searching exercises

--- src/main/java/com/unibuc/gymapp/repositories/ExerciseRepository.java
+++ src/main/java/com/unibuc/gymapp/repositories/ExerciseRepository.java
@@ -2,11 +2,17 @@ package com.unibuc.gymapp.repositories;
 
 import com.unibuc.gymapp.models.Exercise;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
+
     Optional<Exercise> findByTitle(String title);
+
+    @Query("SELECT e FROM Exercise e WHERE e.title LIKE :title")
+    List<Exercise> searchByTitle(String title);
 }
